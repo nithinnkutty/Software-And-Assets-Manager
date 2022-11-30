49b4b5cfc97670958187d83cc7a6d9997a45c3ed
nithin <nithin.nkutty@gmail.com>
2022-11-30 13:55:10 +0000

    Getting started with Gradle project, Added dependencies

---
+++ SoftwareAndAssetManagementSystem/src/main/java/com/bipsync/softwareandassetmanagementsystem/SoftwareAndAssetManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bipsync.softwareandassetmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareAndAssetManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareAndAssetManagementSystemApplication.class, args);
+	}
+
+}
