25b27dc52d7bea118c9479da885639c9780e3a97
Yanwen <qiuy17@cardiff.ac.uk>
2022-12-02 17:49:10 +0000

    Added functionality to add admin

---
+++ SoftwareAndAssetsManager/src/main/java/com/bipsync/SoftwareAndAssetsManager/Controller/GeneralController.java
@@ -0,0 +1,50 @@
+package com.bipsync.SoftwareAndAssetsManager.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class GeneralController {
+/**
+     * This is controller for all the pages
+     */
+    /*
+    Navigating to home
+     */
+
+//Do not use it when the setviewname is not underlined!!!!!!
+
+    /*
+        Navigating to Search
+         */
+    @RequestMapping(path="/Site/EmployeeSearch", method = RequestMethod.GET)
+    public ModelAndView studentSearch(){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("EmployeeSearchForm");
+        return mav;
+    }
+
+//    /*
+//        Navigating to Add Admin
+//         */
+//    @RequestMapping(path="/Site/AddEmployee", method = RequestMethod.GET)
+//    public ModelAndView addEmployee(){
+//        ModelAndView mav = new ModelAndView();
+//        mav.setViewName("AddEmployeeForm");
+//        return mav;
+//    }
+
+    /*
+       Assign this address to Sample page
+        */
+    @RequestMapping(path="/Site/Sample", method = RequestMethod.GET)
+    public ModelAndView SampleFunction(){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("Sample");
+        return mav;
+    }
+
+}
