22215d139ecd8da8242254f2bcff31655f45d6b7
Nithin Narayanan Kutty <narayanankuttyn@cardiff.ac.uk>
2022-12-02 11:21:36 +0000

    Created fragments for head, header and nav bar

---
+++ SoftwareAndAssetManagementSystem/src/main/java/com/bipsync/softwareandassetmanagementsystem/Controller/GeneralController.java
@@ -0,0 +1,19 @@
+package com.bipsync.softwareandassetmanagementsystem.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class GeneralController {
+
+    @RequestMapping(path="/Home")
+    public ModelAndView homePage(){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("home");
+        return mav;
+    }
+
+}
