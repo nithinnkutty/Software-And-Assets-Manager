424382ab0f2f8fa97ca1e6ed746b76ab5fbcd1d2
C22075095 <noyese@cardiff.ac.uk>
2022-12-05 10:13:58 +0000

    Added automatic re-direction upon login depending on role

--- SoftwareAndAssetManagementSystem/src/main/java/com/bipsync/softwareandassetmanagementsystem/Controller/templateController.java
+++ SoftwareAndAssetManagementSystem/src/main/java/com/bipsync/softwareandassetmanagementsystem/Controller/templateController.java
@@ -1,8 +1,14 @@
 package com.bipsync.softwareandassetmanagementsystem.Controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 // This controller is used to handle GET requests from which one can receive the post of the login form.
 
@@ -13,6 +19,19 @@ public class templateController {
     @GetMapping("/login")
     public String getLoginView() {
         return "login";
+    }
+
+    @RequestMapping("/success")
+    public void loginPageRedirect(HttpServletRequest request, HttpServletResponse response, Authentication authResult) throws IOException, ServletException, IOException {
+
+        String role =  authResult.getAuthorities().toString();
+
+        if(role.contains("GENERALADMIN")){
+            response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/generalAdmin/view"));
+        }
+        else if(role.contains("SUPERADMIN")) {
+            response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/superAdmin/view"));
+        }
     }
 
 }
