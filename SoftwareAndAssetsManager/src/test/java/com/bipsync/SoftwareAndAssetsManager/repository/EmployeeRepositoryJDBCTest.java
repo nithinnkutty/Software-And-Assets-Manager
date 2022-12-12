376792811821cd214427ad99892f122b6db0a9f4
Yanwen <qiuy17@cardiff.ac.uk>
2022-12-12 12:04:16 +0000

    Added Some Junit tests for controllers

---
+++ SoftwareAndAssetsManager/src/test/java/com/bipsync/SoftwareAndAssetsManager/repository/EmployeeRepositoryJDBCTest.java
@@ -0,0 +1,31 @@
+package com.Bipsync.SoftwareAndAssetsManager.repository;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import javax.sql.DataSource;
+
+import static org.junit.Assert.*;
+// how to write test for this type of classes?
+public class EmployeeRepositoryJDBCTest {
+    @Test
+    public void findAllEmployee() {
+      }
+
+    @Test
+    public void addEmployee() {
+    }
+
+    @Test
+    public void editEmployee() {
+    }
+
+    @Test
+    public void deleteEmployee() {
+    }
+}
