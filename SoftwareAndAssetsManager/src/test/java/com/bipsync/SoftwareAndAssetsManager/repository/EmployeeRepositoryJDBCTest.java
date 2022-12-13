29169204531844b8343f7cfed6bb9fcd9502504c
Yanwen <qiuy17@cardiff.ac.uk>
2022-12-13 20:41:29 +0000

    applied Junit4 test

--- SoftwareAndAssetsManager/src/test/java/com/bipsync/SoftwareAndAssetsManager/repository/EmployeeRepositoryJDBCTest.java
+++ SoftwareAndAssetsManager/src/test/java/com/bipsync/SoftwareAndAssetsManager/repository/EmployeeRepositoryJDBCTest.java
@@ -1,5 +1,7 @@
 package com.Bipsync.SoftwareAndAssetsManager.repository;
 
+import com.Bipsync.SoftwareAndAssetsManager.DTO.EmployeeDTO;
+import com.Bipsync.SoftwareAndAssetsManager.model.EmployeeMapper;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +12,22 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 import javax.sql.DataSource;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 // how to write test for this type of classes?
+
 public class EmployeeRepositoryJDBCTest {
+
+
+    @Autowired private JdbcTemplate jdbcTemplate;
+
     @Test
     public void findAllEmployee() {
+
+        assertNotNull(jdbcTemplate.query(
+                "select ID,firstName,surname,username,Password,Department,authority,region from employees" ,
+                new EmployeeMapper()));
       }
 
     @Test
